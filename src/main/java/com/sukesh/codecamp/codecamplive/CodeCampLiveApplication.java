57d7dc9e9378e1829cd31c66421b4b2c432fd801
sukeshrs <radhas1@nationwide.com>
2017-07-13 14:15:03 -0400

    Initial commit

---
+++ src/main/java/com/sukesh/codecamp/codecamplive/CodeCampLiveApplication.java
@@ -0,0 +1,12 @@
+package com.sukesh.codecamp.codecamplive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCampLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCampLiveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sukesh/codecamp/codecamplive/CodeCampLiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sukesh.codecamp.codecamplive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeCampLiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
